71b92a22f8fa8c0c0ec6064f40c0194eb42723f5
Swift Kim <swift.kim@samsung.com>
2023-01-11 19:38:10 +0900

    Copy

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,62 +1,71 @@
+import 'dart:ffi';
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-import 'package:flutter/services.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _ffiCallbackPlugin = FfiCallback();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _ffiCallbackPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+Future<void> main() async {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
       ),
-    );
+      body: Center(),
+    ),
+  ));
+
+  print("Dart: Setup.");
+  final initializeApi = dl.lookupFunction<IntPtr Function(Pointer<Void>),
+      int Function(Pointer<Void>)>("NativeInitializeDartApi");
+  initializeApi(NativeApi.initializeApiDLData);
+
+  final interactiveCppRequests = ReceivePort()..listen(requestExecuteCallback);
+  final int nativePort = interactiveCppRequests.sendPort.nativePort;
+  registerCallback1(nativePort, callback1FP);
+  print("Dart: Tell C to start worker threads.");
+  startWorkSimulator();
+
+  // We need to yield control in order to be able to receive messages.
+  while (numCallbacks1 < 1) {
+    print("Dart: Yielding (able to receive messages on port).");
+    await Future.delayed(Duration(seconds: 1));
   }
+  print("Dart: Received expected number of callbacks.");
+
+  interactiveCppRequests.close();
+  print("Dart: Done.");
 }
+
+int numCallbacks1 = 0;
+
+int callback1(int a) {
+  print("Dart:     callback1($a).");
+  numCallbacks1++;
+  return a + 3;
+}
+
+void requestExecuteCallback(dynamic message) {
+  final int work_address = message;
+  final work = Pointer<Work>.fromAddress(work_address);
+  print("Dart:   Calling into C to execute callback ($work).");
+  executeCallback(work);
+  print("Dart:   Done with callback.");
+}
+
+final callback1FP = Pointer.fromFunction<IntPtr Function(IntPtr)>(callback1, 0);
+
+final dl = DynamicLibrary.process();
+
+final registerCallback1 = dl.lookupFunction<
+        Void Function(Int64 sendPort,
+            Pointer<NativeFunction<IntPtr Function(IntPtr)>> functionPointer),
+        void Function(int sendPort,
+            Pointer<NativeFunction<IntPtr Function(IntPtr)>> functionPointer)>(
+    'RegisterMyCallbackBlocking');
+
+final startWorkSimulator =
+    dl.lookupFunction<Void Function(), void Function()>('StartWorkSimulator');
+
+final executeCallback = dl.lookupFunction<Void Function(Pointer<Work>),
+    void Function(Pointer<Work>)>('ExecuteCallback');
+
+class Work extends Opaque {}
--- tizen/src/ffi_callback_plugin.cc
+++ tizen/src/ffi_callback_plugin.cc
@@ -1,71 +1,102 @@
 #include "ffi_callback_plugin.h"
 
-// For getPlatformVersion; remove unless needed for your plugin implementation.
-#include <system_info.h>
-
-#include <flutter/method_channel.h>
 #include <flutter/plugin_registrar.h>
-#include <flutter/standard_method_codec.h>
 
+#include <condition_variable>
+#include <iostream>
 #include <memory>
-#include <string>
+#include <mutex>
+#include <thread>
 
-#include "log.h"
+#include "dart_api_dl.h"
 
 namespace {
 
-class FfiCallbackPlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrar *registrar) {
-    auto channel =
-        std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-            registrar->messenger(), "ffi_callback",
-            &flutter::StandardMethodCodec::GetInstance());
+intptr_t (*my_callback_blocking_fp_)(intptr_t);
+Dart_Port my_callback_blocking_send_port_;
 
-    auto plugin = std::make_unique<FfiCallbackPlugin>();
+typedef std::function<void()> Work;
 
-    channel->SetMethodCallHandler(
-        [plugin_pointer = plugin.get()](const auto &call, auto result) {
-          plugin_pointer->HandleMethodCall(call, std::move(result));
-        });
+// Notify Dart through a port that the C lib has pending async callbacks.
+//
+// Expects heap allocated `work` so delete can be called on it.
+//
+// The `send_port` should be from the isolate which registered the callback.
+void NotifyDart(Dart_Port send_port, const Work* work) {
+  const intptr_t work_addr = reinterpret_cast<intptr_t>(work);
+  printf("C   :  Posting message.\n");
 
-    registrar->AddPlugin(std::move(plugin));
+  Dart_CObject dart_object;
+  dart_object.type = Dart_CObject_kInt64;
+  dart_object.value.as_int64 = work_addr;
+
+  const bool result = Dart_PostCObject_DL(send_port, &dart_object);
+  if (!result) {
+    printf("C   :  Posting message to port failed.\n");
   }
+  printf("C   :  Posted message to port.\n");
+}
 
-  FfiCallbackPlugin() {}
-
-  virtual ~FfiCallbackPlugin() {}
-
- private:
-  void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-    const auto &method_name = method_call.method_name();
-
-    // Replace "getPlatformVersion" check with your plugin's method.
-    if (method_name == "getPlatformVersion") {
-      char *value = nullptr;
-      int ret = system_info_get_platform_string(
-          "http://tizen.org/feature/platform.version", &value);
-      if (ret == SYSTEM_INFO_ERROR_NONE) {
-        result->Success(flutter::EncodableValue(std::string(value)));
-      } else {
-        result->Error(std::to_string(ret), "Failed to get platform version.");
-      }
-      if (value) {
-        free(value);
-      }
-    } else {
-      result->NotImplemented();
-    }
+// Do a callback to Dart in a blocking way, being interested in the result.
+//
+// Dart returns `a + 3`.
+intptr_t MyCallbackBlocking(intptr_t a) {
+  std::mutex mutex;
+  std::unique_lock<std::mutex> lock(mutex);
+  intptr_t result;
+  auto callback = my_callback_blocking_fp_;  // Define storage duration.
+  std::condition_variable cv;
+  bool notified = false;
+  const Work work = [a, &result, callback, &cv, &notified]() {
+    result = callback(a);
+    printf("C Da:     Notify result ready.\n");
+    notified = true;
+    cv.notify_one();
+  };
+  const Work* work_ptr = new Work(work);  // Copy to heap.
+  NotifyDart(my_callback_blocking_send_port_, work_ptr);
+  printf("C   :  Waiting for result.\n");
+  while (!notified) {
+    cv.wait(lock);
   }
-};
+  printf("C   :  Received result.\n");
+  return result;
+}
+
+// Simulated work for Thread #1.
+//
+// Simulates heavy work with sleeps.
+void Work1() {
+  printf("C T1: Work1 Start.\n");
+  std::thread::id this_id = std::this_thread::get_id();
+  std::cout << "Thread " << this_id << " ...\n";
+
+  const intptr_t val1 = 3;
+  const intptr_t val2 = MyCallbackBlocking(val1);  // val2 = 6.
+  printf("C T1: MyCallbackBlocking returned %d.\n", val2);
+}
 
 }  // namespace
 
-void FfiCallbackPluginRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar) {
-  FfiCallbackPlugin::RegisterWithRegistrar(
-      flutter::PluginRegistrarManager::GetInstance()
-          ->GetRegistrar<flutter::PluginRegistrar>(registrar));
+DART_EXPORT FLUTTER_PLUGIN_EXPORT void RegisterMyCallbackBlocking(
+    Dart_Port send_port, intptr_t (*callback1)(intptr_t)) {
+  my_callback_blocking_fp_ = callback1;
+  my_callback_blocking_send_port_ = send_port;
 }
+
+DART_EXPORT FLUTTER_PLUGIN_EXPORT void StartWorkSimulator() {
+  printf("C Da:  Starting SimulateWork.\n");
+  std::thread* thread = new std::thread(Work1);
+  printf("C Da:  Started SimulateWork.\n");
+}
+
+DART_EXPORT FLUTTER_PLUGIN_EXPORT void ExecuteCallback(Work* work_ptr) {
+  printf("C Da:    ExecuteCallback.\n");
+  const Work work = *work_ptr;
+  work();
+  delete work_ptr;
+  printf("C Da:    ExecuteCallback done.\n");
+}
+
+void FfiCallbackPluginRegisterWithRegistrar(
+    FlutterDesktopPluginRegistrarRef registrar) {}
